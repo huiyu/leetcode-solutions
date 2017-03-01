635734cfa8a0acafb6650aa084232a52b5e312ce
JaniceLian <lianjinfei@gmail.com>
2017-03-01 10:56:32 +0800

    format code

--- algorithms/001/Solution.js
+++ algorithms/001/Solution.js
@@ -8,17 +8,15 @@
  *
  */
 var twoSum = function(nums, target) {
-    var length = nums.length;
-    var comp = [];
-    for(i=0;i<length;i++) {
-        var idx = comp.indexOf(nums[i]),
-            num2 = target - nums[i];
-            // console.log("num2:"+num2 + "   idx2:"+idx)
-        if(idx == -1){
-            comp.push(num2);
-        }
-        else {
-            return [nums.indexOf(num2), i]
-        }
+  var length = nums.length;
+  var comp = [];
+  for (i = 0; i < length; i++) {
+    var idx = comp.indexOf(nums[i]),
+      num2 = target - nums[i];
+    if (idx == -1) {
+      comp.push(num2);
+    } else {
+      return [nums.indexOf(num2), i]
     }
+  }
 };
